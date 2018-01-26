b4a59d6594105f0515a94ed35ef23d971b809a0d
Richard Dyer <Richard@r7development.com>
2018-01-25 17:15:28 -0800

    Updating psd and saving default view

--- Client/MainTabBarController.swift
+++ Client/MainTabBarController.swift
@@ -68,7 +68,7 @@ class MainTabBarController: UITabBarController {
         if(firstLaunch)
         {
             firstLaunch=false
-            self.selectedIndex = 4 //default is 2/Top
+            self.selectedIndex = 2 //default is 2/Top
         }
     }
 }
